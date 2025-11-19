e9874d626612648d0d1392be09260eacf12b1a19
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-11-19 17:18:28 +0600

    (feat) implement modal for cart and movie details

--- src/part_2/Header.jsx
+++ src/part_2/Header.jsx
@@ -4,10 +4,13 @@ import sun from "../assets/icons/sun.svg";
 import Logo from "../assets/logo.svg";
 import ring from "../assets/ring.svg";
 import shoppingCart from "../assets/shopping-cart.svg";
+import CartDetails from "./cinema/CartDetails";
 const Header = () => {
-  const [darkMood, setDarkMood] = useState(true);
+  const [darkMood, setDarkMood] = useState(false);
+  const [showCart, setShowCart] = useState(false);
   return (
     <header>
+      {showCart && <CartDetails onClose={() => setShowCart(false)} />}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a href="index.html">
           <img src={Logo} width="139" height="26" alt="" />
@@ -34,6 +37,7 @@ const Header = () => {
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
               href="#"
+              onClick={() => setShowCart(true)}
             >
               <img src={shoppingCart} width="24" height="24" alt="" />
             </a>
