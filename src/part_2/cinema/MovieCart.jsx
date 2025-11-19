35017372dd673ff6017613a5f7e1da8e68bbdf21
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-11-19 16:20:56 +0600

    (feat) movie details modal

--- src/part_2/cinema/MovieCart.jsx
+++ src/part_2/cinema/MovieCart.jsx
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { getImageUrl } from "../../utils/cine-utility";
+import MovieDetailsModal from "./MovieDetailsModal";
 import Ratings from "./Ratings";
 
 const MovieCart = ({ movie }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const handleMovieSelection = (movie) => {
+    setSelectedMovie(movie);
+    setShowModal(true);
+  };
+
+  const handleShowModal = () => {
+    setSelectedMovie(null);
+    setShowModal(false);
+  };
   return (
-    <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-      <img
-        className="w-full object-cover"
-        src={getImageUrl(movie.cover)}
-        alt=""
-      />
-      <figcaption className="pt-4">
-        <h3 className="text-xl mb-1">{movie.title}</h3>
-        <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
-        <div className="flex items-center space-x-1 mb-5">
-          <Ratings value={movie.rating} />
-        </div>
-        <a
-          className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-          href="#"
-        >
-          <img src="./assets/tag.svg" alt="" />
-          <span>${movie.price} | Add to Cart</span>
-        </a>
-      </figcaption>
-    </figure>
+    <>
+      {showModal && (
+        <MovieDetailsModal movie={selectedMovie} onClose={handleShowModal} />
+      )}
+      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+        <button onClick={() => handleMovieSelection(movie)}>
+          <img
+            className="w-full object-cover"
+            src={getImageUrl(movie.cover)}
+            alt=""
+          />
+          <figcaption className="pt-4">
+            <h3 className="text-xl mb-1">{movie.title}</h3>
+            <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+            <div className="flex items-center space-x-1 mb-5">
+              <Ratings value={movie.rating} />
+            </div>
+            <a
+              className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+              href="#"
+            >
+              <img src="./assets/tag.svg" alt="" />
+              <span>${movie.price} | Add to Cart</span>
+            </a>
+          </figcaption>
+        </button>
+      </figure>
+    </>
   );
 };
 
